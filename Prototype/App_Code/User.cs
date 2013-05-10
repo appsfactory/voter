1c0d54e57eaa906e75e0ba06fc1c422ee28bc56c
unknown <JoseJonathan.Puertos@R8K12KT.corp.ad.ctc>
2013-05-10 11:19:51 -0400

    Admin pages have now connectivity to server

---
+++ Prototype/App_Code/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for Survey
+/// </summary>
+/// 
+[Serializable()]
+public class User
+{
+    public String user;
+    public String password;
+    public String token;
+    public bool isAdmin;
+   
+
+	public User()
+	{
+       
+		
+	}
+
+}
